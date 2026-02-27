e0cedf12b89ba7a6e80860e8046e997a4638f632
Boughpohpue <joseph.marianson@gmail.com>
2026-02-27 09:21:10 +0100

    Updated just.js for Enum changes
    Updated marquee

--- js/just/1.0.1/just.js
+++ js/just/1.0.1/just.js
@@ -318,9 +318,49 @@ export class Reflector {
 /* >>>---> enum.js >----------------------------------------------------------------> */
 /* Just an enum */
 export class Enum {
-  constructor() {
-    if (this.constructor._sealed)
-      throw new Error(`Can't construct Enum instance from outside an enum!`);
+  #value = undefined;
+
+  get value() { return this.#value; }
+  get valueType() { return typeof this.#value; }
+
+  constructor(value = undefined) {
+    const _ctor = this.constructor;
+    if (_ctor._sealed)
+      throw new Error("An Enum instance can be created only inside the enum class!");
+    if (value === null || value === undefined) return;
+    for (const [name, instance] of Object.entries(_ctor)) {
+      if (!(instance instanceof _ctor)) continue;
+      if (instance.value === value)
+        throw new Error("An Enum instance with the same value already exists!");
+      if (instance.valueType && instance.valueType !== typeof value)
+        throw new Error("An Enum value type must be consistent across the enum class!");
+    }
+    this.#value = value;
+    Object.freeze(this);
+  }
+
+  static parse(e) {
+    if (e === undefined || e === null) return;
+    let valuesType = undefined;
+    const upper = typeof e === "string" ? e.toUpperCase() : undefined;
+    for (const [name, instance] of Object.entries(this)) {
+      if (!(instance instanceof this)) continue;
+      if (upper === name.toUpperCase()) return instance;
+      if (instance.value === e) return instance;
+      if (!valuesType && instance.valueType) valuesType = instance.valueType;
+    }
+    if (valuesType === typeof e) return e;
+  }
+  static ensure(e) {
+    if (e === undefined || e === null) return;
+    if (e instanceof this) return e;
+    return this.parse(e);
+  }
+  static getValue(e, fallback = undefined) {
+    const ensured = this.ensure(e) ?? this.ensure(fallback);
+    return ensured instanceof this
+      ? ensured.value
+      : ensured;
   }
   static seal() {
     this._sealed = true;
